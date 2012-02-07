b6c28b9ebc6831615b40dabc75e02706251d7d47
Mauricio Mantish <mauriciomantilla@gmail.com>
2012-02-07 11:21:41 -0500

    insert image conserving aspect ratio

--- js/async-media-load.js
+++ js/async-media-load.js
@@ -19,12 +19,18 @@
 
 		var insertImg = function($a, id, width, height)
 		{
-			var img_src = 'http://img.youtube.com/vi/'+id+'/default.jpg';
-
-			var $img = $('<img src="'+img_src+'"/>');
-			$img.width(width).height(height).css({position: 'absolute', top: '0', left: '0'});
-			$a.css({position: 'relative'});
-			$a.append($img);
+			var img_src = 'http://img.youtube.com/vi/'+id+'/hqdefault.jpg';
+			var image = document.createElement('img');
+			image.onload = function(){
+				var img_height = this.height*width/this.width;
+				var img_top = (height - img_height)/2;
+				var $img = $(this);
+				$img.width(width).height(img_height).css({position: 'absolute', top: img_top+'px', left: '0'});
+				$a.css({position: 'relative'});
+				$a.parent().css({overflow: 'hidden'});
+				$a.append($img);
+			}
+			image.src = img_src;
 		}
 
 		var playYoutube = function(e)
