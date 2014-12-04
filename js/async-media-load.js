ff94ea0c5417a46152c57c0bf9d0b7cd438853e3
Carlos Sosa <cesosag@gmail.com>
2014-12-04 15:00:41 -0500

    Responsive feature

--- js/async-media-load.js
+++ js/async-media-load.js
@@ -17,18 +17,47 @@
 			return (match && match[1]) || null;
 		}
 
-		var insertImg = function($a, img_src, width, height){
+		var insertImg = function($a, img_src, width, height, provider){
 			var image = document.createElement('img');
 			image.onload = function(){
 				var img_height = this.height*width/this.width;
 				var img_top = (height - img_height)/2;
 				var $img = $(this);
-				$img.width(width).height(img_height).css({
-					position: 'absolute',
-					top: img_top +'px',
-					left: '0'
-				});
-				$a.css({position: 'relative'}).parent().css({overflow: 'hidden'});
+				if(opts.responsive){
+					console.log(provider);
+					$img.css({
+						left: 0,
+						margin: '-9% 0',
+						position: 'absolute',
+						top: 0,
+						width: '100%'
+					});
+					if(provider == 'vimeo_l'){
+						$img.css({margin: 0});
+					}
+					$a.css({
+						display: 'block',
+						position: 'relative',
+					}).parent().css({
+						height: 0,
+						'max-width': '100%',
+						'padding-bottom': '56.25%',
+						overflow: 'hidden',
+						position: 'relative',
+					});
+				}
+				else{
+					$img.width(width).height(img_height).css({
+						position: 'absolute',
+						top: img_top +'px',
+						left: '0'
+					});
+					$a.css({
+						position: 'relative'
+					}).parent().css({
+						overflow: 'hidden'
+					});
+				}
 				$a.append($img);
 			}
 			image.src = img_src;
@@ -42,11 +71,13 @@
 				success: function(data){
 					if(width > 200){
 						var img_src = data[0].thumbnail_large;
+						var prov_s = 'vimeo_l';
 					}
 					else{
 						var img_src = data[0].thumbnail_medium;
+						var prov_s = 'vimeo_m';
 					}
-					insertImg($a, img_src, width, height);
+					insertImg($a, img_src, width, height, prov_s);
 				}
 			});
 		}
@@ -62,7 +93,13 @@
 			else{
 				embed_url = 'http://www.youtube.com/embed/'+e.data.id+'?rel=0&amp;autoplay=1&amp;wmode=opaque';
 			}
-			var $embed = $('<iframe width="'+e.data.width+'" height="'+e.data.height+'" src="'+embed_url+'" frameborder="0" allowfullscreen></iframe>');
+			if(opts.responsive){
+				var style = 'height:100%;left:0;position:absolute;top:0;width:100%;';
+				var $embed = $('<iframe style="'+style+'" src="'+embed_url+'" frameborder="0" allowfullscreen></iframe>');
+			}
+			else{
+				var $embed = $('<iframe width="'+e.data.width+'" height="'+e.data.height+'" src="'+embed_url+'" frameborder="0" allowfullscreen></iframe>');
+			}
 			$(this).replaceWith($embed);
 		}
 
@@ -81,7 +118,25 @@
 				}
 				else{
 					var img_src = 'http://img.youtube.com/vi/'+video_id+'/hqdefault.jpg';
-					insertImg($this, img_src, width, height);
+					insertImg($this, img_src, width, height, 'youtube');
+				}
+			}
+			else{
+				if(opts.responsive){
+					$(this).find('img').css({
+						left: 0,
+						position: 'absolute',
+						top: 0,
+						width: '100%'
+					}).parent().css({
+						display: 'block'
+					}).parent().css({
+						height: 0,
+						'max-width': '100%',
+						'padding-bottom': '56.25%',
+						overflow: 'hidden',
+						position: 'relative',
+					});
 				}
 			}
 
